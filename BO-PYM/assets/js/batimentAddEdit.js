cc357f53f7b8c12ee8082b9a4a131cbf7e4138db
Nguyen Marc <nguyen_marc@live.fr>
2020-05-07 19:12:37 +0200

    UI Fix

--- BO-PYM/assets/js/batimentAddEdit.js
+++ BO-PYM/assets/js/batimentAddEdit.js
@@ -1,4 +1,11 @@
-require('../css/batimentAddEdit.css');
-require('bootstrap');
-require('jquery');
-require('popper.js');
+import '../css/batimentAddEdit.css';
+import 'bootstrap';
+import $ from 'jquery';
+import 'popper.js';
+
+$(document).ready(function () {
+  $('input[type="file"]').change(function (e) {
+    const fileName = e.target.files[0].name;
+    $('.custom-file-label').html(fileName);
+  });
+});
--- BO-PYM/assets/js/entrepriseNew.js
+++ BO-PYM/assets/js/entrepriseNew.js
@@ -1,3 +1,10 @@
-require('../css/entrepriseNew.css');
-require('bootstrap');
-require('jquery');
+import '../css/entrepriseNew.css';
+import 'bootstrap';
+import $ from 'jquery';
+
+$(document).ready(function () {
+  $('input[type="file"]').change(function (e) {
+    const fileName = e.target.files[0].name;
+    $('.custom-file-label').html(fileName);
+  });
+});

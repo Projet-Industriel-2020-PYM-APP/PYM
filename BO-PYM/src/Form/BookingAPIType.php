e00accc2c68a7a70028ab301c07f29e5ee574860
Nguyen Marc <nguyen_marc@live.fr>
2020-06-11 02:04:38 +0200

    [booking] API Updated

--- BO-PYM/src/Form/BookingAPIType.php
+++ BO-PYM/src/Form/BookingAPIType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Booking;
 use App\Form\DataTransformer\ServiceToIDTransformer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\DataTransformer\BooleanToStringTransformer;
 use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToStringTransformer;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,11 +15,13 @@ class BookingAPIType extends AbstractType
 {
     private $serviceTransformer;
     private $dateTimeTransformer;
+    private $booleanTransformer;
 
     public function __construct(ServiceToIDTransformer $serviceToIDTransformer)
     {
         $this->serviceTransformer = $serviceToIDTransformer;
-        $this->dateTimeTransformer =  new DateTimeToStringTransformer('Europe/Paris', "UTC", "Y-m-d\TH:i:s.u\Z");
+        $this->dateTimeTransformer = new DateTimeToStringTransformer('Europe/Paris', "UTC", "Y-m-d\TH:i:s.u\Z");
+        $this->booleanTransformer = new BooleanToStringTransformer('true', ['false', 'null']);
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -26,17 +29,25 @@ class BookingAPIType extends AbstractType
         $builder->add('title', TextType::class)
             ->add('start_date', TextType::class, [
                 'property_path' => 'startDate',
+                'invalid_message' => '"start_date" est erroné.',
             ])
             ->add('end_date', TextType::class, [
                 'property_path' => 'endDate',
+                'invalid_message' => '"end_date" est erroné.',
             ])
             ->add('service_id', TextType::class, [
                 'property_path' => 'service',
+                'invalid_message' => '"service_id" est erroné.',
+            ])
+            ->add('superpose', TextType::class, [
+                'required' => true,
+                'invalid_message' => '"superpose" est erroné.',
             ]);
 
         $builder->get('service_id')->addModelTransformer($this->serviceTransformer);
         $builder->get('start_date')->addModelTransformer($this->dateTimeTransformer);
         $builder->get('end_date')->addModelTransformer($this->dateTimeTransformer);
+        $builder->get('superpose')->addModelTransformer($this->booleanTransformer);
     }
 
 
